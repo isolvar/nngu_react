8aae67ff5efcd4d5b17c835e6cd0952b5bc9804b
Alexey Shemanov <schemanov2012@yandex.ru>
2022-05-14 12:57:44 +0300

    added lesson 3

--- src/shared/Header/index.tsx
+++ src/shared/Header/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import style from './Header.module.scss';
 
 const Header = () => {
-  return <div className={style.page_header}>Шапка сайта</div>;
+  return <div className={style.page_header}>Header</div>;
 };
 
 export default Header;
